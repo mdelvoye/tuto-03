af377a556c172b2b31e6c92b20d7e2b99f77b4a7
Michael Delvoye <mdelvoye@MacBook-Pro-de-Michael.local>
2017-07-13 14:43:10 +0200

    Création des fichiers du tuto pages statiques

---
+++ application/controllers/Pages.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mdelvoye
+ * Date: 13/07/2017
+ * Time: 11:18
+ */
+
+class Pages extends CI_Controller {
+
+    public function view($page = 'home')
+    {
+        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+        {
+            // Whoops, we don't have a page for that!
+            show_404();
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
